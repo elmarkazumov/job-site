4f44dbb0c75e4e174634775c47e50d249dbf5ab8
elmarkazumov <elmar.kazumov20@gmail.com>
2023-06-12 21:19:12 +0500

    functional upload resume

--- Controller/guestController.js
+++ Controller/guestController.js
@@ -1,14 +1,17 @@
 const response = require('../response')
 const db = require('../settings/db')
 const localStorage = require("localStorage")
+const alert = require('alert')
+const fileUpload = require('express-fileupload');
+
 
 exports.getAllVacancy = (req, res) => {
     db.query("SELECT * FROM `Vacancy`", (error, rows, fields) => {
         if (error) {
-            console.log(400, error, res);
-          } else {
-            res.render('index', {root: "./", data: rows})
-          }
+                console.log(400, error, res);
+            } else {
+                res.render('index', {root: "./", data: rows})
+            }
     })
 }
 
@@ -18,27 +21,34 @@ exports.redirectVacancy = (req, res) => {
 }
 
 exports.renderSelectedVacancy = (req, res) => {
-  db.query("SELECT * FROM `Vacancy` WHERE `id` = '" + localStorage.getItem('id') + "'", (error, rows, fields) =>{
+    db.query("SELECT * FROM `Vacancy` WHERE `id` = '" + localStorage.getItem('id') + "'", (error, rows, fields) =>{
     if(error){
-      console.log(error);
+        console.log(error);
     } else{
-      res.render('vacancyForm', {root: "./", data: rows});
-      const row = JSON.parse(JSON.stringify(rows));
-      row.map(vacancy => {
-        localStorage.setItem('selectedVacancyName', vacancy.name);
-      })
+        res.render('vacancyForm', {root: "./", data: rows});
+        const row = JSON.parse(JSON.stringify(rows));
+        row.map(vacancy => {
+            localStorage.setItem('selectedVacancyName', vacancy.name);
+        })
     }
-  })
+    })
 }
 
 exports.sendForm = (req, res) => {
-  const sql = "INSERT INTO resume (name, email, phone, file, selectedVacancyName, status) VALUES ('"+ req.body.nameUser + "','" + req.body.email + "','" + req.body.phone + "','"
-   + req.body.file + "','" + localStorage.getItem('selectedVacancyName') + "','" + "В ожидании" +"')";
-  db.query(sql, (error, results) => {
-      if(error) {
-          response.status(400, error, res);
-      } else{
-          alert('резюме отправлено!');
-      }
-  })
+    const sql = "INSERT INTO resume (name, email, phone, file, selectedVacancyName, status) VALUES ('"+ req.body.nameUser + "','" + req.body.email + "','" + req.body.phone + "','"
+    + Date.now() + '-' + req.files.fileResume.name + "','" + localStorage.getItem('selectedVacancyName') + "','" + "В ожидании" +"')";
+    db.query(sql, (error, results) => {
+        if(error) {
+            response.status(400, error, res);
+        } else{
+            let upfile = req.files.fileResume;
+            let updest = "./uploads/" + Date.now() + '-' + upfile.name;
+
+            upfile.mv(updest, err => {
+                if (err) { return res.status(500).send(err); }
+            });
+            alert('резюме отправлено!');
+            res.redirect('/vacancyForm');
+        }
+    })
 }
--- server.js
+++ server.js
@@ -1,18 +1,21 @@
-const express = require('express')
-const app = express()
-const port = process.env.PORT || 3000
-const bodyParser = require('body-parser')
-const path = require('path')
-const cookieParser = require('cookie-parser')
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+const bodyParser = require('body-parser');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const fileUpload = require('express-fileupload');
 
-app.use(bodyParser.urlencoded({extended: true}))
-app.use(bodyParser.json())
-app.use(cookieParser())
+app.use(fileUpload());
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+app.use(cookieParser());
 
 app.use(express.static('./views'));
 
-const routes = require('./settings/routes')
-routes(app)
+const routes = require('./settings/routes');
+routes(app);
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
